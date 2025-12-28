737e8430fc2ee5ef8d35f26df93f0f0f3ac90455
anandb7 <anandswaroopbehara@gmail.com>
2025-12-29 02:10:08 +0530

    Initial Commit

---
+++ AnniFlix/AnniFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnniFlixApp.swift
+//  AnniFlix
+//
+//  Created by Anand Behara on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct AnniFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnniFlix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnniFlix
+//
+//  Created by Anand Behara on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
