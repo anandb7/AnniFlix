9c9c2d15b1aad0cc7cabf5ebbfb3123a6d6f3d98
anandb7 <anandswaroopbehara@gmail.com>
2025-12-29 22:29:35 +0530

    Feat: Separated Constants file for cleaner code

---
+++ AnniFlix/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  AnniFlix
+//
+//  Created by Anand Behara on 12/29/25.
+//
+
+import Foundation
+
+struct Constants{
+    static let homeString = "Home"
+    static let upcomingString = "Upcoming"
+    static let searchString = "Search"
+    static let downloadString = "Download"
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
+    
+    static let testTitleURL = "https://www.researchgate.net/profile/Stefan-Bolea/publication/309717420/figure/fig1/AS:425102790664192@1478363811754/Fight-Club-movie-poster-1999.png"
+}
