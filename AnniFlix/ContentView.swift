84cd1e658cb09fef293d073a4e171cdbc5b79509
anandb7 <anandswaroopbehara@gmail.com>
2025-12-29 22:02:19 +0530

    Add: Navbar for the application

--- AnniFlix/ContentView.swift
+++ AnniFlix/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world! hi how are you doing today")
+        TabView{
+            Tab("Home", systemImage: "house"){
+                Text("Home")
+            }
+            Tab("Upcoming", systemImage: "play.circle"){
+                Text("Upcoming")
+            }
+            Tab("Search", systemImage: "magnifyingglass"){
+                Text("Search")
+            }
+            Tab("Download", systemImage: "arrow.down.to.line"){
+                Text("Download")
+            }
         }
-        .padding()
     }
 }
 
