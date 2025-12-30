e821468aa6467ffca6e4cd3bddde542011fbed48
anandb7 <anandswaroopbehara@gmail.com>
2025-12-30 15:23:59 +0530

    Add: Horizontal View Panel for 3 movie posters

--- AnniFlix/Constants.swift
+++ AnniFlix/Constants.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Constants{
     static let homeString = "Home"
@@ -13,11 +14,30 @@ struct Constants{
     static let searchString = "Search"
     static let downloadString = "Download"
     static let playString = "Play"
+    static let trendingMovieString = "Trending Movies"
     
     static let homeIconString = "house"
     static let upcomingIconString = "play.circle"
     static let searchIconString = "magnifyingglass"
     static let downloadIconString = "arrow.down.to.line"
     
-    static let testTitleURL = "https://www.researchgate.net/profile/Stefan-Bolea/publication/309717420/figure/fig1/AS:425102790664192@1478363811754/Fight-Club-movie-poster-1999.png"
+    static let testTitleURL = "https://image.tmdb.org/t/p/original/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
+    
+    static let testTitleURL2 = "https://image.tmdb.org/t/p/original/602vevIURmpDfzbnv5Ubi6wIkQm.jpg"
+    
+    static let testTitleURL3 = "https://image.tmdb.org/t/p/original/oix0aNv1lvW3nUGspUyvSIBlpbs.jpg"
 }
+
+extension Text {
+    func ghostButton() -> some View {
+        self
+            .frame(width: 100, height: 50)
+            .foregroundStyle(.buttonText)
+            .bold()
+            .background {
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .stroke(.buttonBorder, lineWidth: 5)
+            }
+    }
+}
+
