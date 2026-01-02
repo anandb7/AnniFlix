27fc066166741bafe56af1ce9cc5a1d3204cf164
anandb7 <anandswaroopbehara@gmail.com>
2026-01-02 20:30:03 +0530

    add: top rated movies, tv shows from the movie db api

--- AnniFlix/HomeView.swift
+++ AnniFlix/HomeView.swift
@@ -55,9 +55,12 @@ struct HomeView: View {
                         }
                         
                         HorizontalListView(header: Constants.trendingMovieString, titles: viewModel.trendingMovies)
-    //                    HorizontalListView(header: Constants.trendingTVString)
-    //                    HorizontalListView(header: Constants.topRatedMovieString)
-    //                    HorizontalListView(header: Constants.topRatedTVString)
+                        HorizontalListView(header:Constants.trendingTVString,
+                            titles: viewModel.trendingTV)
+                        HorizontalListView(header: Constants.topRatedMovieString,
+                        titles: viewModel.topRatedMovies)
+                        HorizontalListView(header: Constants.topRatedTVString,
+                        titles: viewModel.topRatedTV)
                     }
                 case .failed(let error):
                     Text(error.localizedDescription)
