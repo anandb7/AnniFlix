315a476d5693023ef5b2ef7bfbd2406ac6436173
anandb7 <anandswaroopbehara@gmail.com>
2026-01-06 03:18:29 +0530

    Add: YT trailer fetching UI

--- AnniFlix/DataFetcher.swift
+++ AnniFlix/DataFetcher.swift
@@ -10,6 +10,8 @@ import Foundation
 struct DataFetcher {
     let tmdbBaseURL = APIConfig.shared?.tmdbBaseURL
     let tmdbAPIKey = APIConfig.shared?.tmdbAPIKey
+    let youtubeSearchURL = APIConfig.shared?.youtubeSearchURL
+    let youtubeAPIKey = APIConfig.shared?.youtubeAPIKey
     
     //https://api.themoviedb.org/3/trending/movie/day?api_key=YOUR_API_KEY
     //https://api.themoviedb.org/3/movie/top_rated?api_key=YOUR_API_KEY
@@ -21,8 +23,40 @@ struct DataFetcher {
         }
         
         print(fetchTitlesURL)
+        var titles = try await fetchAndDecode(url: fetchTitlesURL, type: TMDBAPIObject.self).results
+       
+        Constants.addPosterPath(to: &titles)
+        return titles
+    }
+    
+    
+    
+    //https://www.googleapis.com/youtube/v3/search?q=Breaking%20Bad%20trailer&key=APIKEY
+    func fetchVideoId(for title: String) async throws -> String {
+        guard let baseSearchURL = youtubeSearchURL else {
+            throw NetworkError.missingConfig
+        }
         
-        let(data,urlResponse) = try await URLSession.shared.data(from: fetchTitlesURL)
+        guard let searchAPIKey = youtubeAPIKey else {
+            throw NetworkError.missingConfig
+        }
+        
+        let trailerSearch = title + YoutubeURLStrings.space.rawValue + YoutubeURLStrings.trailer.rawValue
+        
+        guard let fetchVideoURL = URL(string: baseSearchURL)?.appending(queryItems: [
+            URLQueryItem(name: YoutubeURLStrings.queryShorten.rawValue, value: trailerSearch),
+            URLQueryItem(name: YoutubeURLStrings.key.rawValue, value: searchAPIKey)
+        ]) else {
+            throw NetworkError.urlBuildFailed
+        }
+        
+        print(fetchVideoURL)
+        
+        return try await fetchAndDecode(url: fetchVideoURL, type: YoutubeSearchResponse.self).items?.first?.id?.videoId ?? ""
+    }
+    
+    func fetchAndDecode<T: Decodable>(url: URL, type: T.Type) async throws -> T {
+        let(data,urlResponse) = try await URLSession.shared.data(from: url)
         
         guard let response = urlResponse as? HTTPURLResponse, response.statusCode == 200 else {
             throw NetworkError.badURLResponse(underlyingError: NSError(
@@ -33,9 +67,7 @@ struct DataFetcher {
         
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        var titles = try decoder.decode(APIObject.self, from: data).results
-        Constants.addPosterPath(to: &titles)
-        return titles
+        return try decoder.decode(type, from: data)
     }
     
     private func buildURL(media:String,type:String) throws -> URL? {
--- AnniFlix/ViewModel.swift
+++ AnniFlix/ViewModel.swift
@@ -16,12 +16,15 @@ class ViewModel {
         case failed(underlyingError: Error)
     }
     private(set) var homeStatus: FetchStatus = .notStarted
+    private(set) var videoIdStatus: FetchStatus = .notStarted
+    
     private let dataFetcher = DataFetcher()
     var trendingMovies: [Title] = []
     var trendingTV: [Title] = []
     var topRatedMovies: [Title] = []
     var topRatedTV: [Title] = []
     var heroTitle = Title.previewTitles[0]
+    var videoId = ""
     
     func getTitles() async {
         homeStatus = .fetching
@@ -48,6 +51,18 @@ class ViewModel {
             }
         } else {
             homeStatus = .success
+        }
+    }
+    
+    func getVideoId(for title: String) async {
+        videoIdStatus = .fetching
+        
+        do {
+            videoId = try await dataFetcher.fetchVideoId(for: title)
+            videoIdStatus = .success
+        } catch {
+            print(error)
+            videoIdStatus = .failed(underlyingError: error)
         }
     }
 }
