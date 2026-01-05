52dc214a52ae1a2bc83bbd2dda394e9e7e18db6e
anandb7 <anandswaroopbehara@gmail.com>
2026-01-06 02:09:44 +0530

    Introduced State navigation in home view

---
+++ AnniFlix/TitleDetailView.swift
@@ -0,0 +1,42 @@
+//
+//  TitleDetailView.swift
+//  AnniFlix
+//
+//  Created by Anand Behara on 1/3/26.
+//
+
+import SwiftUI
+
+struct TitleDetailView: View {
+    let title: Title
+    
+    var body: some View {
+       GeometryReader { geometry in
+           ScrollView {
+               LazyVStack(alignment: .leading) {
+                   AsyncImage(url: URL(string: title.posterPath ?? "")) { image in
+                       image
+                           .resizable()
+                           .scaledToFit()
+                   } placeholder: {
+                       ProgressView()
+                   }
+                   .frame(width: geometry.size.width, height: geometry.size.height * 0.85)
+                   
+                   Text((title.name ?? title.title) ?? "")
+                       .bold()
+                       .font(.title2)
+                       .padding(5)
+                    
+                   
+                   Text(title.overview ?? "")
+                       .padding(5)
+               }
+           }
+        }
+    }
+}
+
+#Preview {
+    TitleDetailView(title: Title.previewTitles[0])
+}
